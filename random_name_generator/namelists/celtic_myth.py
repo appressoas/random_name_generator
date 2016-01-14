62f11195caf098cdbe57fae596f2769e1994196d
Magne Westlie <magne@appresso.no>
2016-01-14 13:59:44 +0100

    Added some more names.

---
+++ random_name_generator/namelists/celtic_myth.py
@@ -0,0 +1,4 @@
+names = [
+    'Airmid', 'Artio', 'Balor', 'Branwen', 'Camalus', 'Cerunnos', 'Cyhiraeth', 'Druantia', 'Giobhniu', 'Lugh', 'Llyr',
+    'Maeve', 'Manannan', 'Margawse', 'Mebd', 'Mider', 'Morrigan', 'Nemain',
+]
---
+++ random_name_generator/namelists/irish_myth.py
@@ -0,0 +1,3 @@
+names = [
+    'Aine', 'Angus', 'Og', 'Anu', 'Babd', 'Catha', 'Bel', 'Bran', 'Brighid', 'Bris', 'Dagda', 'Danu', 'Diancecht',
+    'Flidais', 'Labraid', 'Macha', 'Niamh']
